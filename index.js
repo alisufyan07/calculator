f25b4072da3d68d18f383037b74b83730249e5ac
alisufyan07 <alisuf07@gmail.com>
2024-03-31 12:49:21 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+//SHABANG
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division",
+            "Percentage",
+        ],
+    },
+]);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Percentage" &&
+    answer.firstNumber <= answer.secondNumber) {
+    console.log((answer.firstNumber / answer.secondNumber) * 100, "% of", answer.secondNumber);
+}
+else if (answer.operator === "Percentage" && answer.firstNumber > answer.secondNumber) {
+    console.log("first number must be less than second number");
+}
+console.log("Thanks");
